11964dff1fdde49ea8e4eba9ec30e2f7a813b80c
João Pedro Almeida Menino <joaopedroalmeidamenino@gmail.com>
2024-09-08 15:03:26 -0300

    funções basicas ok

--- app.py
+++ app.py
@@ -2,12 +2,25 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/', methods = ['GET','POST'])
-def calculadora ():
+@app.route('/', methods=['GET', 'POST'])
+def calculadora():
+    display = ""
+    
     if request.method == 'POST':
-        pass
-    return render_template("main.html")
+        button = request.form.get('button')
+        current_display = request.form.get('display', '')
+
+        if button == "=":
+            try:
+                display = str(eval(current_display))
+            except Exception as e:
+                display = ""
+        elif button == "del":
+            pass
+        else:
+            display = current_display + button
+
+    return render_template("main.html", display=display)
 
 if __name__ == "__main__":
-    app.run(debug = True)
-    
+    app.run(debug=True)
