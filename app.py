f361d611bf7bb5ed7d8f415f4f42f0c4bf41a17f
JOAO PEDRO ALMEIDA MENINO <00001132521804sp@aluno.educacao.sp.gov.br>
2024-09-10 21:16:17 -0300

    bruno

--- app.py
+++ app.py
@@ -4,41 +4,66 @@ import formulas as fm
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET','POST'])
 def calculadora():
-    display = ""
-    
-    if request.method == 'POST':
-        button = request.form.get('button')
-        current_display = request.form.get('display', '')
+    try:
+        display = ""
+        
+        if request.method == 'POST':
+            button = request.form.get('button')
+            current_display = request.form.get('display', '')
 
-        if button == "=":
-            try:
-                display = str(eval(current_display))
-            except Exception as e:
-                display = ""
-        elif button == "del":
-            display = current_display[:-1]
-        elif button == "pi":
-            display = current_display + fm.pi()
-        elif button == "raiz":
-            numero = int(current_display)
-            display = fm.raiz(numero)
-        elif button in ["sen","cos","tg"]:
-            try:
-                numero = float(current_display)
-                if button == "sen":
-                    display = fm.sen(numero)
-                elif button == "cos":
-                    display = fm.cos(numero)
+            if button == "=":
+                try:
+                    display = fm.calculator_basic(current_display)
+                except Exception as e:
+                    display = ""
+            elif button in ["del","C"]:
+                if button == "del":
+                    display = current_display[:-1]
                 else:
-                    display = fm.tg(numero)
-            except:
-                display = ""
-        else:
-            display = current_display + button
+                    display = ""
+            elif button in ["pi","raiz","fatorial"]:
+                if button == "pi":
+                    display = current_display + fm.pi()
+                elif button == "raiz":
+                    try:
+                        numero = int(current_display)
+                        display = fm.raiz(numero)
+                    except:
+                        display = ""
+                else:
+                    try:
+                        numero = int(current_display)
+                        display = fm.factorial(numero)
+                    except: 
+                        display = ""
+            elif button in ["log","in"]:
+                try:
+                    numero = int(current_display)
+                    if button == "in":
+                        display = fm.IN(numero)
+                    else:
+                        display = fm.log(numero)
+                except:
+                    display = ""
+            elif button in ["sen","cos","tg"]:
+                try:
+                    numero = float(current_display)
+                    if button == "sen":
+                        display = fm.sen(numero)
+                    elif button == "cos":
+                        display = fm.cos(numero)
+                    else:
+                        display = fm.tg(numero)
+                except:
+                    display = ""
+            else:
+                display = current_display + button
 
-    return render_template("main.html", display=display)
+        return render_template("main.html", display=display)
+    except Exception as e:
+        return "<h1>ERROR</h1>", e
 
 if __name__ == "__main__":
     app.run(debug=True)
--- formulas.py
+++ formulas.py
@@ -1,5 +1,14 @@
 import math as mt 
 
+def calculator_basic(expression):
+    return str(eval(expression))
+
+def infinite(num):
+    return mt.isfinite(num)
+
+def combination(k, n):
+    return mt.comb(k, n)
+
 def pi():
     return str(mt.pi)
 
@@ -14,3 +23,32 @@ def tg(num):
 
 def raiz(num):
     return str(mt.sqrt(num))
+
+def log(num):
+    return str(mt.log10(num))
+
+def IN(num):
+    return str(mt.log(num))
+
+def factorial(num):
+    return str(mt.factorial(num))
+
+def radians_degrees(num):
+    return mt.degrees(num)
+
+def degrees_radians(num):
+    return mt.radians(num)
+
+def triangle_area(base, heigth):
+    return base * heigth / 2
+
+def rectangle_area(base, heigth):
+    return base * heigth
+
+#financial sector
+def ROI(revenue, expenses):
+    return (revenue - expenses) / expenses
+
+#phamaceutical sector
+def dose_medication(weigth):
+    return weigth * 2
