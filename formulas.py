c6a281fb4852ac285fb9726c81415bea4c52f164
JOAO PEDRO ALMEIDA MENINO <00001132521804sp@aluno.educacao.sp.gov.br>
2024-09-17 18:43:02 -0300

    Science calculator

--- formulas.py
+++ formulas.py
@@ -9,6 +9,9 @@ def infinite(num):
 def combination(k, n):
     return mt.comb(k, n)
 
+def percentage(num):
+    return str(num/100)
+
 def pi():
     return str(mt.pi)
 
@@ -21,7 +24,7 @@ def cos(num):
 def tg(num):
     return str(mt.tan(mt.radians(num)))
 
-def raiz(num):
+def square_root(num):
     return str(mt.sqrt(num))
 
 def log(num):
@@ -52,3 +55,32 @@ def ROI(revenue, expenses):
 #phamaceutical sector
 def dose_medication(weigth):
     return weigth * 2
+
+
+def identify_button(button,current_display, display):
+    if button == "=":
+        try:
+            display = calculator_basic(current_display)
+            return display
+        except Exception as e:
+            display = ""
+            return display
+    elif button in ["del","CE"]:
+        if button == "del":
+            display = current_display[:-1]
+            return display
+        else:
+            display = ""
+            return display
+    elif button == "%":
+        try:
+            numero = int(current_display)
+            display = percentage(numero)
+            return display
+        except:
+            display = ''
+            return display
+        
+    else:
+        display = current_display + button
+        return display
